6a711dc7a6e24d981345a4ff2d4220eaec79b141
Joseph Surla <josephsurla@gmail.com>
2017-05-24 16:36:58 -0700

    Add post route to to instantiate user to database and route to user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+
+
   get '/signup' do
+    @user = User.new
     erb :'/users/new'
+  end
+
+  post '/signup' do
+    @user = User.new(email: params[:email], password: params[:password])
+    @user.save
+    erb :'/users/index'
   end
 end
