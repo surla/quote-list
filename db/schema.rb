71cdf7235c267fd7d3abe61dd34e2e642a9435ed
Joseph Surla <josephsurla@gmail.com>
2017-05-29 17:01:48 -0700

    Change email to username in users table

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170524232049) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "username"
     t.string "password_digest"
   end
 
