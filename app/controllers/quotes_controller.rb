b1c070eebf3708c7d7ee9b36d7499271f95658e1
Joseph Surla <josephsurla@gmail.com>
2017-06-09 08:57:47 -0700

    Working quotes pages. Able to add and view quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,20 @@
 class QuotesController < ApplicationController
+
+  get '/quotes' do
+    @quotes = Quote.all
+    erb :'/quotes/index'
+  end
+
   get '/quotes/new' do
     erb :'/quotes/new'
   end
 
   post '/quotes/new' do
-    @quote = Quote.new(quote: params[:quotes])
-    if @quote
-      erb :'/quotes/index'
+    @quote = Quote.new(quote: params[:quote])
+    @quote.users << current_user
+    @quote.author = Author.find_or_create_by(author_name: params[:author_name])
+    if @quote.save
+      erb :'/quotes/show'
     else
       erb :'/quotes/new'
     end
--- app/models/quotes.rb
+++ app/models/quotes.rb
@@ -2,5 +2,8 @@ class Quote < ActiveRecord::Base
   belongs_to :author
   belongs_to :topic
 
+  has_many :user_quotes
+  has_many :users, through: :user_quotes
+
   validates_presence_of :quote
 end
