65b1dd613eadde43d9e986212dfd179546835e71
Joseph Surla <josephsurla@gmail.com>
2017-06-09 08:18:32 -0700

    Helper to check for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  helpers do
+   def current_user
+     @user || User.find_by_id(session[:user_id])
+   end
+  end
 end
