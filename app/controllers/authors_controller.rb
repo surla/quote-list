f494b5efce590e4925ef8bf1209c2f78539df913
Joseph Surla <josephsurla@gmail.com>
2017-06-09 09:35:22 -0700

    Create authors index and show page

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,11 @@
+class AuthorsController < ApplicationController
+  get '/authors' do
+    @authors = Author.all
+    erb :'authors/index'
+  end
+
+  get '/authors/:id' do
+    @author = Author.find(params[:id])
+    erb :'/authors/show'
+  end
+end
