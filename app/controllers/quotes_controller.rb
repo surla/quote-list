94c99161402dc3522f4661617dda6c730f8c5d4b
Joseph Surla <josephsurla@gmail.com>
2017-06-15 13:17:26 -0700

    Create patch route and edit view for user to edit quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,15 +5,27 @@ class QuotesController < ApplicationController
     erb :'/quotes/index'
   end
 
+  get '/quotes/:id' do
+    @quote = Quote.find(params[:id])
+    erb :'/quotes/show'
+  end
+
   get '/quotes/new' do
+    @quote = Quote.new
     erb :'/quotes/new'
   end
+
+  get '/quotes/:id/edit' do
+    @quote = Quote.find(params[:id])
+    erb :'/quotes/edit'
+  end
+
 
   post '/quotes/new' do
     @quote = Quote.new(quote: params[:quote])
     @quote.users << current_user
-    @quote.author = Author.find_or_create_by(name: params[:author_name])
-    @quote.topic = Topic.find_or_create_by(name: params[:topic_name])
+    @quote.author = Author.find_or_create_by(name: params[:author])
+    @quote.topic = Topic.find_or_create_by(name: params[:topic])
     if @quote.save
       erb :'/quotes/show'
     else
@@ -21,10 +33,18 @@ class QuotesController < ApplicationController
     end
   end
 
-  get '/quotes/:id' do
+  patch '/quotes/:id' do
     @quote = Quote.find(params[:id])
-    erb :'/quotes/show'
+    @quote.author = Author.find_or_create_by(name: params[:author])
+    @quote.topic = Topic.find_or_create_by(name: params[:topic])
+    if @quote.save
+      erb :'/quotes/show'
+    else
+      erb :'/quotes/edit'
+    end
   end
+
+
 
   post '/quotes/:id/delete' do
     @quote = Quote.find(params[:id])
