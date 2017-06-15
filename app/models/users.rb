aade81ac67cb57a36253438d606963d7752e00ff
Joseph Surla <josephsurla@gmail.com>
2017-06-15 12:57:33 -0700

    Add validation to signup user form and include error message in view

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :username, :password, :password_confirmation, presence: true
+
   has_many :user_quotes
   has_many :quotes, through: :user_quotes
 
