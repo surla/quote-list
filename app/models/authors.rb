6445e831c96a170d93ef69cbfe16ec0967ad9a73
Joseph Surla <josephsurla@gmail.com>
2017-06-18 22:12:04 -0700

    Add validations and checks presence of name.

--- app/models/authors.rb
+++ app/models/authors.rb
@@ -1,3 +1,5 @@
 class Author < ActiveRecord::Base
   has_many :quotes
+
+  validates :name, presence: true
 end
--- app/models/topics.rb
+++ app/models/topics.rb
@@ -1,3 +1,5 @@
 class Topic < ActiveRecord::Base
   has_many :quotes
+
+  validates :name, presence: true
 end
